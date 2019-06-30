a920fb761e6899e045cd421317d82d506ffeba2e
Ha Tran <8clubs@gmail.com>
2019-07-01 02:05:42 +0700

    fix test tax.

--- tests/App/TaxTest.php
+++ tests/App/TaxTest.php
@@ -8,43 +8,41 @@ use PHPUnit\Framework\TestCase;
 
 class TaxTest extends TestCase
 {
-    public $tax;
-
-    public function __construct()
-    {
-        $this->tax = new Tax(20);
-    }
-
     public function testTaxInit()
     {
-        $this->assertNotEmpty($this->tax);
-        $this->assertInstanceOf(Tax::class, $this->tax);
-        $this->assertEquals(20, $this->tax->taxAmount);
+        $tax = new Tax(20);
+        $this->assertNotEmpty($tax);
+        $this->assertInstanceOf(Tax::class, $tax);
+        $this->assertEquals(20, $tax->taxAmount);
     }
 
     public function testTaxAdd()
     {
-        $this->tax->add(2);
-        $this->assertEquals(22, $this->tax->taxAmount);
+        $tax = new Tax(20);
+        $tax->add(2);
+        $this->assertEquals(22, $tax->taxAmount);
     }
 
     public function testTaxSubtract()
     {
-        $this->tax->subtract(2);
-        $this->assertEquals(18, $this->tax->taxAmount);
+        $tax = new Tax(20);
+        $tax->subtract(2);
+        $this->assertEquals(18, $tax->taxAmount);
     }
 
     public function testTaxCalculateForEmployeeHaveLessThanTwoKids()
     {
-        $taxFee = $this->tax->calculateTax(1000);
+        $tax = new Tax(20);
+        $taxFee = $tax->calculateTax(1000);
         $this->assertEquals(200, $taxFee);
     }
 
     public function testTaxCalculateForEmployeeHaveMoreThanTwoKids()
     {
         // decrease 2% tax
-        $this->tax->subtract(2);
-        $taxFee = $this->tax->calculateTax(1000);
+        $tax = new Tax(20);
+        $tax->subtract(2);
+        $taxFee = $tax->calculateTax(1000);
         $this->assertEquals(180, $taxFee);
     }
 }
